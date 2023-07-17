3e0ebad5cdace8e3332f3dd41a46670ff957e4ec
jmerazo <jmerazo96@gmail.com>
2023-07-16 23:44:20 -0500

    fixed autoplay slider and select search

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,72 @@
 <script setup>
-import { computed, onMounted } from "vue";
-import { RouterLink, useRoute } from "vue-router";
+import { ref, computed, onMounted, createElementBlock } from "vue";
+import { RouterLink, useRoute, useRouter } from "vue-router";
 import {useFamiliasStore} from '../stores/families'
+import {useEspeciesStore} from '../stores/species'
+import Alerta from '../components/Alerta.vue'
+
 import Glide from '@glidejs/glide'
 import '@glidejs/glide/dist/css/glide.core.css'
 import '@glidejs/glide/dist/css/glide.theme.css'
 
 const route = useRoute();
+const router = useRouter();
 const paginaInicio = computed(() => route.name === "home");
 
-const familias = useFamiliasStore()
+const familias = useFamiliasStore();
+const especies = useEspeciesStore()
 
 onMounted(() => {
-    const glide = new Glide('.glide', {
-        type: 'carousel',
-        autoplay: 5000
-    });
+    setTimeout(() => {
+    new Glide('.glide', {
+      type: 'carousel',
+      autoplay: 5000
+    }).mount();
+  }, 0);
 });
+
+const error = ref('')
+
+const categoria = ref('')
+const valorBuscar = ref('')
+
+const validarcampos = ()=>{
+  
+  if(categoria.value){
+
+    if(categoria.value === 'Familia' && valorBuscar.value ){
+      msgFamilia()
+      return
+    }else if(categoria.value === 'Especie' && valorBuscar.value){
+      msgComun(valorBuscar.value)
+      return
+    }else if(categoria.value === 'Nombre científico' && valorBuscar.value){
+      msgCientifico(valorBuscar.value)
+      return
+    }else{
+      error.value = 'Todos los campos son obligatorios'
+      setTimeout(()=>{
+        error.value = ''
+      },3000)
+  }
+
+  }else{
+    error.value = 'Todos los campos son obligatorios'
+    setTimeout(()=>{
+      error.value = ''
+    },3000)
+  }
+}
+
+const msgFamilia = ()=>{
+  router.push('/familias');
+}
+const msgComun = (comun)=>{
+  router.push('/especies');
+}
+const msgCientifico = (cientifico)=>{
+  alert(cientifico)
+}
 </script>
 
 <template>
@@ -95,42 +145,99 @@ onMounted(() => {
             </div>
             <form
                 v-if="paginaInicio"
-                class="md:w-1/2 2xl:w-1/3 bg-green-700 my-32 p-10 rounded-lg shadow space-y-6 bg-opacity-30 form-blur"
+                class="md:w-1/2 2xl:w-1/3 bg-green-700 my-32 p-10 rounded-lg shadow space-y-6 bg-opacity-30 form-blur .form"
                 @submit.prevent="handleSubmit"
             >
-                <div class="space-y-4">
-                    <label
-                        class="block text-white uppercase font-extrabold text-lg"
-                        for="ingrediente">Buscar</label>
-                        <select 
-                        id="categoria"
-                        class="text-base p-3 w-full rounded-lg focus:outline-none font-bold"
-                        
-                    >
-                        <option value="">Seleccione una opción... </option>
-                        <option> Familia </option>
-                        <option> Nombre común </option>
-                        <option> Nombre científico </option>
-                        <!-- <option v-for="familia in familias.familias" :key="familia.familias"> {{familia.familia}} </option> -->
-                    </select>
-                </div>
+            <Alerta v-if="error">
+        {{ error }}
+      </Alerta>
+        <div class="space-y-4">
+          <label
+            class="block text-white uppercase font-extrabold text-lg"
+            for="ingrediente"
+            >Buscar</label
+          >
+          <select
+            id="categoria"
+            class="text-2xl p-3 w-full rounded-lg focus:outline-none font-bold"
+            v-model="categoria"
+            @click="valorBuscar=''"
+          >
+            <option value="" >-- Seleccione --</option>
+            <option>Familia</option>
+            <option>Especie</option>
+            <option>Nombre científico</option>
+            
+          </select>
+        </div>
 
-                <div class="space-y-4">
-                    <label
-                        class="block text-white uppercase font-extrabold text-lg"
-                        for="parametro">Valor a buscar</label>
-                    <input 
-                        id="parametro"
-                        class="text-base p-3 w-full rounded-lg focus:outline-none"
-                        placeholder="Familia, nombre científico o nombre común"
-                    >
-                </div>
-                    
-                <input
-                    type="submit"
-                    class="bg-button-search cursor-pointer  font-extrabold w-full p-2 rounded-lg uppercase"
-                    value="Buscar"
-                />
+        <div v-if="categoria ==='Familia'" class="space-y-4">
+          <label
+            class="block text-white uppercase font-extrabold text-lg"
+            for="parametro"
+            >Valor a buscar</label
+          >
+          <select
+            id="categoria"
+            class="text-2xl p-3 w-full rounded-lg focus:outline-none font-bold"
+            v-model="valorBuscar"
+          >
+            <option value="">-- Seleccione --</option>
+            <option v-for="familia in familias.familias" :key="familia.familia"> {{ familia.familia }}</option>
+          </select>
+        </div>
+        <div v-if="categoria ==='Especie'" class="space-y-4">
+          <label
+            class="block text-white uppercase font-extrabold text-lg"
+            for="parametro"
+            >Valor a buscar</label
+          >
+          <select
+            id="categoria"
+            class="text-2xl p-3 w-full rounded-lg focus:outline-none font-bold"
+            v-model="valorBuscar"
+          >
+            <option value="">-- Seleccione --</option>
+            <option v-for="especie in especies.especies" :key="especie.nom_comunes"> {{ especie.nom_comunes }}</option>
+          </select>
+        </div>
+        <div v-if="categoria ==='Nombre científico'" class="space-y-4">
+          <label
+            class="block text-white uppercase font-extrabold text-lg"
+            for="parametro"
+            >Valor a buscar</label
+          >
+          <select
+            id="categoria"
+            class="text-2xl p-3 w-full rounded-lg focus:outline-none font-bold"
+            v-model="valorBuscar"
+          >
+            <option value="">-- Seleccione --</option>
+            <option v-for="especie in especies.especies" :key="especie.nombre_cientifico"> {{ especie.nombre_cientifico }}</option>
+          </select>
+        </div>
+        <div v-if="!categoria" class="space-y-4">
+          <label
+            class="block text-white uppercase font-extrabold text-lg"
+            for="parametro"
+            >Valor a buscar</label
+          >
+          <select
+            disabled
+            id="categoria"
+            class="text-2xl p-3 w-full rounded-lg focus:outline-none font-bold"
+            
+          >
+            <option value="">-- Seleccione --</option>
+            
+          </select>
+        </div>
+        <input
+          type="submit"
+          class="bg-lime-400 hover:bg-lime-500 cursor-pointer font-extrabold w-full p-2 rounded-lg uppercase"
+          value="Buscar"
+          @click="validarcampos()"
+        />
             </form>
         </div>
 
@@ -163,7 +270,7 @@ onMounted(() => {
     .header-slider .glide__slide img {
         object-fit: cover;
         width: 100%;
-        height: 850px;
+        height: 860px;
     }
 
     .form-blur {
