595f62bca11aa2d13aee92e4575281f7ce50d765
jmerazo <55777009+jmerazo@users.noreply.github.com>
2023-08-06 15:57:55 -0500

    filter and search glossary

--- src/stores/glossary.js
+++ src/stores/glossary.js
@@ -1,19 +1,47 @@
-import {ref, onMounted, reactive} from 'vue'
+import {ref, onMounted, computed, watch } from 'vue'
 import {defineStore} from 'pinia'
-import {useModalStore} from './modal'
 import APIService from '../services/APIService'
 
 export const useGlossaryStore = defineStore('glossary',()=>{
     
-    const modal = useModalStore()
     const glossary = ref([])
+    const selectedLetter = ref(null);
+    const searchTerm = ref('');
 
     onMounted(async ()=>{
         const {data} = await APIService.getGlossary()
         glossary.value = data
     })
 
+    function getFilteredGlossary() {
+        if (selectedLetter.value !== null) {
+            const letter = selectedLetter.value.toLowerCase();
+            return glossary.value.filter((term) => term.word.toLowerCase().startsWith(letter));
+        } else if (searchTerm.value !== '') {
+            const search = searchTerm.value.toLowerCase();
+            return glossary.value.filter(
+              (term) =>
+                term.word.toLowerCase().includes(search)
+            );
+        } else {
+            return glossary.value;
+        }
+    }
+    
+    function setSelectedLetter(letter) {
+        selectedLetter.value = letter;
+    }
+
+    function setSearchTerm(term) {
+        searchTerm.value = term;
+    }
+
     return {
-        glossary
+        glossary,
+        selectedLetter,
+        searchTerm,
+        setSelectedLetter,
+        setSearchTerm,
+        getFilteredGlossary
     }
 })
--- src/views/GlossaryView.vue
+++ src/views/GlossaryView.vue
@@ -1,28 +1,55 @@
 <script setup>
-import {useGlossaryStore} from '../stores/glossary'
+import { computed } from "vue";
+import { useGlossaryStore } from '../stores/glossary';
+const glossary = useGlossaryStore();
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+/* const searchTerm = computed(() => glossary.searchTerm); */
 
-const glossary = useGlossaryStore()
-
+const filteredGlossary = computed(() => glossary.getFilteredGlossary());
+const searchTerm = computed({
+   get: () => glossary.searchTerm,
+   set: (value) => glossary.setSearchTerm(value),
+});
 </script>
 
 <template>
     <div class="w-auto mx-auto">
-        <h1 class="text-6xl mb-10 mt-20 text-center font-extrabold">Glosario</h1>
-        <hr>
-        <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 mt-10 gap-10 my-10">
-            <div class="bg-customGreen rounded-lg p-4 shadow-lg backdrop-filter backdrop-blur-md bg-opacity-20"
-                v-for="g in glossary.glossary"
-                :key="g.id"
-            >
-                <strong class="font-bold">{{ g.word }}:</strong> {{ g.definition }}
-            </div>
-            
+      <h1 class="text-6xl mb-10 mt-20 text-center font-extrabold">Glosario</h1>
+      <div class="flex justify-center my-5">
+        <div class="mx-2">
+          <button
+            class="bg-customGreen hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            @click="() => glossary.setSelectedLetter(null)"
+          >
+            Todas
+          </button>
         </div>
-
+        <div class="mx-2" v-for="letter in alphabet" :key="letter">
+          <button
+            class="bg-customGreen hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            @click="() => glossary.setSelectedLetter(letter)"
+          >
+            {{ letter }}
+          </button>
+        </div>
+        <div class="mx-2">
+          <input
+            type="text"
+            class="border-2 border-gray-300 py-2 px-4 rounded-lg w-64"
+            placeholder="Buscar..."
+            :value="searchTerm"
+            @input="($event) => searchTerm = $event.target.value"
+          />
+        </div>
+      </div>
+      <hr>
+    <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 mt-10 gap-10 my-10">
+        <div
+            class="bg-customGreen rounded-lg p-4 shadow-lg backdrop-filter backdrop-blur-md bg-opacity-20"
+            v-for="g in filteredGlossary" 
+            :key="g.id"> 
+            <strong class="font-bold">{{ g.word }}:</strong> {{ g.definition }} 
+        </div> 
+        </div> 
     </div>
-
 </template>
-
-<style>
-
-</style>
