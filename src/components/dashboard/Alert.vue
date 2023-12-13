b3e689dd4724f75a4c28e396a6b3a90179af7706
ordonez93 <90074302+ordonez93@users.noreply.github.com>
2023-12-13 14:18:52 -0500

    filtroFecha monitoreos especie

--- src/components/dashboard/Alert.vue
+++ src/components/dashboard/Alert.vue
@@ -8,10 +8,8 @@
 
 <style scoped>
 .alerta{
-   padding: 1rem;
-   font-weight: 900;
-   text-transform: uppercase;
-   font-size: 1.6rem;
+   font-weight: 700;
+   font-size: 1rem;
    text-align: center; 
    background-color: var(--blanco);
    border-left: .5rem solid #b91c1c;
--- src/views/dashboardviews/MonitoringSpecieView.vue
+++ src/views/dashboardviews/MonitoringSpecieView.vue
@@ -1,127 +1,374 @@
 <script setup>
-import {ref} from 'vue'
-import Alerta from '@/components/dashboard/Alert.vue';
+import { ref } from "vue";
+import Alerta from "@/components/dashboard/Alert.vue";
 import { useConsultaStore } from "@/stores/consulta";
-
 
 const consulta = useConsultaStore();
 
-const fechaInicial = ref('')
-const fechaFinal = ref('')
-const error = ref('')
+const fechaInicial = ref("");
+const fechaFinal = ref("");
+const error = ref("");
 
-const limpiar = ()=>{
-  fechaInicial.value = '';
-  fechaFinal.value = '';
-  consulta.limpiarFiltroFecha()
-}
+const limpiar = () => {
+  fechaInicial.value = "";
+  fechaFinal.value = "";
+  consulta.limpiarFiltroFecha();
+};
 
-const consultarFechas = ()=>{
-  if(!fechaFinal.value || !fechaInicial.value){
-    
-    error.value = 'Debe ingresar un rango de fechas'
-    setTimeout(()=>{
-            error.value = ''
-        },3000)
-    return
+const consultarFechas = () => {
+  if (!fechaFinal.value || !fechaInicial.value) {
+    error.value = "Debe ingresar un rango de fechas";
+    setTimeout(() => {
+      error.value = "";
+    }, 3000);
+    return;
   }
-  if(fechaInicial.value > fechaFinal.value){
-    error.value = 'La fecha inicial no puede ser mayor a la fecha final'
-    setTimeout(()=>{
-            error.value = ''
-        },3000)
-    return
+  if (fechaInicial.value > fechaFinal.value) {
+    error.value = "La fecha inicial no puede ser mayor a la fecha final";
+    setTimeout(() => {
+      error.value = "";
+    }, 3000);
+    return;
   }
-  consulta.filtrarFecha(fechaInicial.value, fechaFinal.value)
-}
+  consulta.filtrarFecha(fechaInicial.value, fechaFinal.value);
+};
 </script>
 
 <template>
-  <h1 class="text-4xl mb-10 mt-10 text-center font-extrabold">
-    Monitoreos de la especie {{ consulta.nombreEspecie }}
-  </h1>
-  <div class="flex flex-col items-center mt-5">
-    <label class="p-3 text-lg font-bold rounded-lg mx-3 w-1/3 lg:w-auto">
-      Filtrar por fecha de monitoreos
-    </label>
-    <div class="flex mt-5 mb-5 space-x-2">
-      <div>
-        <label class="p-3 text-lg font-bold rounded-lg mx-3"
-          >Fecha inicial</label
-        >
-        <input
-          class="p-3 rounded-lg lg:w-3/4 sm:w-full border-2 border-gray-500 py-2 px-4 font-extrabold"
-          type="date"
-          v-model="fechaInicial"
-        />
-      </div>
-      <div>
-        <label class="p-3 text-lg font-bold rounded-lg mx-3">Fecha final</label>
-        <input
-          class="p-3 rounded-lg lg:w-3/4 sm:w-full border-2 border-gray-500 py-2 px-4 font-extrabold"
-          type="date"
-          v-model="fechaFinal"
-        />
-      </div>
-      <div class="lg:flex lg:items-center mt-5 ">
-        <button
-          @click="consultarFechas()" 
-          class="bg-green-600 p-2 rounded-lg text-lg font-bold shadow-lg hover:bg-green-500 ">
-          Filtrar fechas
-        </button>
-      </div>
-      <div v-if="fechaInicial && fechaFinal" class="lg:flex lg:items-center mt-5 ">
-        <button
-          @click="limpiar" 
-          class="bg-red-600 p-2 rounded-lg text-lg font-bold shadow-lg hover:bg-red-500 ">
-          limpiar filtro
-        </button>
+  <div class="contenedor">
+    <!-- encabezado vista -->
+    <h1 class="reporte__heading">
+      Monitoreos de la especie {{ consulta.nombreEspecie }}
+    </h1>
+    <div class="contenido__header">
+      <fieldset class="buscador">
+        <legend class="buscador__titulo">Filtrar fecha de monitoreo</legend>
+
+        <div class="buscador__flex">
+          <div class="buscador__inputs">
+            <div>
+              <label class="buscador__label">Fecha inicial: </label>
+              <input
+                class="buscador__input"
+                type="date"
+                v-model="fechaInicial"
+              />
+            </div>
+            <div>
+              <label class="buscador__label">Fecha final: </label>
+              <input class="buscador__input" type="date" v-model="fechaFinal" />
+            </div>
+          </div>
+
+          <div class="buscador__botones">
+            <button @click="consultarFechas()" class="buscador__boton">
+              Filtrar fechas
+            </button>
+            <button
+              v-if="fechaInicial && fechaFinal"
+              @click="limpiar"
+              class="buscador__boton buscador__boton--limpiar"
+            >
+              limpiar filtro
+            </button>
+          </div>
+        </div>
+        <Alerta v-if="error">
+          <p>{{ error }}</p>
+        </Alerta>
+      </fieldset>
+      <div class="botones__descarga">
+        <a @click="descargarExcel(consulta.datosExcel)" class="boton" href="#"
+          ><font-awesome-icon
+            class="boton__excel"
+            :icon="['fas', 'file-excel']"
+        /></a>
+        <a
+          @click="
+            descargarPdf(
+              consulta.datosExcel,
+              `candidatos de la especie ${consulta.nombreEspecie}`
+            )
+          "
+          class="boton"
+          href="#"
+          ><font-awesome-icon class="boton__pdf" :icon="['fas', 'file-pdf']"
+        /></a>
       </div>
     </div>
   </div>
-  <Alerta v-if="error">
-    <p>{{ error }}</p>
-  </Alerta>
-  <hr />
-  <table class="bg-slate-50 shadow-md border rounded-lg mx-auto mt-5">
-    <thead>
-      <tr>
-        <th class="px-4 py-2 text-gray-600">Fecha monitoreo</th>
-        <th class="px-4 py-2 text-gray-600">Altura total</th>
-        <th class="px-4 py-2 text-gray-600">Altura comercial</th>
-        <th class="px-4 py-2 text-gray-600">Follaje</th>
-        <th class="px-4 py-2 text-gray-600">Tamaño follaje</th>
-        
 
+  <!-- fin encabezado vista -->
+
+  <hr />
+  <main>
+    <table class="tabla">
+    <thead class="tabla__encabezado">
+      <tr class="tabla__fila">
+        <th class="dato__encabezado fecha">Fecha monitoreo</th>
+        <th class="dato__encabezado">Altura total</th>
+        <th class="dato__encabezado">Altura comercial</th>
+        <th class="dato__encabezado">Estado Follaje</th>
+        <th class="dato__encabezado">Tamaño follaje</th>
       </tr>
     </thead>
-    <tbody>
-      <tr v-for="monitoreo in consulta.displayedMonitoring" v-bind:key="monitoreo.IDmonitoreo">
-        <td class="px-4 py-3 border">{{ monitoreo.fecha_monitoreo}}</td>
-        <td class="px-4 py-3 border">{{ monitoreo.altura_total}}</td>
-        <td class="px-4 py-3 border">{{ monitoreo.altura_comercial}}</td>
-        <td class="px-4 py-3 border">{{ monitoreo.follaje}}</td>
-        <td class="px-4 py-3 border">{{ monitoreo.follaje_porcentaje}}</td>
+    <tbody class="tabla__contenido">
+      <tr
+        class="tabla__fila"
+        v-for="monitoreo in consulta.displayedMonitoring"
+        v-bind:key="monitoreo.IDmonitoreo"
+      >
+        <td class="tabla__dato fecha"><span class="nombre__campo">Fecha monitoreo</span> {{ monitoreo.fecha_monitoreo }}</td>
+        <td class="tabla__dato" :class="{sinInfo:!monitoreo.altura_total}"> <span class="nombre__campo">Altura Total: </span>{{ monitoreo.altura_total ? monitoreo.altura_total:'Por evaluar' }}</td>
+        <td class="tabla__dato " :class="{sinInfo:!monitoreo.altura_comercial}"> <span class="nombre__campo">Altura comerical: </span>{{ monitoreo.altura_comercial ?  monitoreo.altura_comercial : 'Por evaluar'}}</td>
+        <td class="tabla__dato" :class="{sinInfo:!monitoreo.follaje}"> <span class="nombre__campo">Follaje estado: </span>{{ monitoreo.follaje ? monitoreo.follaje : 'Por evaluar'}}</td>
+        <td class="tabla__dato tabla__ultimo" :class="{sinInfo:!monitoreo.follaje_porcentaje}"><span class="nombre__campo">Tamaño Follaje: </span>{{ monitoreo.follaje_porcentaje ? monitoreo.follaje_porcentaje : 'Por evaluar' }}</td>
       </tr>
     </tbody>
   </table>
+  </main>
+ 
   <!-- paginador -->
   <div class="flex justify-center mt-5 mb-10">
-      <button
-        v-for="page in consulta.totalPages"
-        :key="page"
-        @click="consulta.changePage(page)"
-        class="px-3 py-2 mx-1 rounded-lg bg-green-200 text-black hover:bg-green-600"
-        :class="{ 'bg-green-600': page === consulta.currentPage }"
-      >
-        {{ page }}
-      </button>
-    </div>
+    <button
+      v-for="page in consulta.totalPages"
+      :key="page"
+      @click="consulta.changePage(page)"
+      class="px-3 py-2 mx-1 rounded-lg bg-green-200 text-black hover:bg-green-600"
+      :class="{ 'bg-green-600': page === consulta.currentPage }"
+    >
+      {{ page }}
+    </button>
+  </div>
 
-    <h1 v-if="consulta.monitoreosEspecie.length == 0" class="text-center font-bold text-2xl mt-5 mb-40">
-      No hay monitoreos para la especie <span class="text-green-900">{{ consulta.nombreEspecie }}</span>
-    </h1>
+  <h1
+    v-if="consulta.monitoreosEspecie.length == 0"
+    class="text-center font-bold text-2xl mt-5 mb-40"
+  >
+    No hay monitoreos para la especie
+    <span class="text-green-900">{{ consulta.nombreEspecie }}</span>
+  </h1>
 </template>
 
 <style  scoped>
+
+/* encabezado de la vista */
+.reporte__heading {
+  font-size: 1rem;
+  margin: 2rem 0 1rem 0;
+}
+@media (min-width: 768px) {
+  .reporte__heading {
+    font-size: 1.5rem;
+    margin: 3rem;
+  }
+}
+.contenido__header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin-bottom: 0.5rem;
+}
+
+/* buscador */
+.buscador {
+  border: 1px solid var(--gris);
+}
+
+.buscador:last-of-type {
+  margin-bottom: 0rem;
+}
+.buscador legend {
+  background-color: var(--primary);
+  width: 100%;
+  text-align: center;
+  color: var(--blanco);
+  text-transform: uppercase;
+  font-weight: 400;
+  padding: 0.1rem;
+  font-size: 0.9rem;
+  margin-bottom: 0.1rem;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+}
+@media (min-width: 768px) {
+  .buscador legend {
+    margin-bottom: 1rem;
+  }
+}
+.buscador__flex {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.8rem;
+}
+.buscador__label {
+  padding: 0;
+  margin: 0;
+  font-size: 0.8rem;
+  font-weight: 700;
+  display: block;
+  text-align: center;
+}
+
+@media (min-width: 768px) {
+  .buscador__label {
+    display: inline;
+    text-align: left;
+    font-size: 1rem;
+  }
+}
+.buscador__inputs {
+  display: flex;
+  font-size: 0.8rem;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 0.5rem;
+}
+@media (min-width: 992px) {
+  .buscador__inputs {
+    flex-direction: row;
+    gap: 1rem;
+  }
+}
+
+.buscador__boton {
+  color: var(--blanco);
+  background-color: var(--primary);
+  padding: 0.2rem 0.4rem;
+  border-radius: 5px;
+  margin-left: 0.5rem;
+  margin-bottom: 0.3rem;
+  transition: background-color 0.3s ease;
+}
+.buscador__boton--limpiar {
+  background-color: rgb(138, 9, 9);
+  opacity: 0.8;
+}
+
+.buscador__boton:hover {
+  background-color: var(--primary-hover);
+}
+.buscador__boton--limpiar:hover {
+  background-color: rgb(138, 9, 9);
+  opacity: 1;
+}
+
+/* descargas */
+.botones__descarga {
+  display: flex;
+  gap: 1rem;
+}
+.boton {
+  font-size: 1.5rem;
+}
+@media (min-width: 768px) {
+  .boton {
+    font-size: 1.8rem;
+  }
+}
+.boton__excel {
+  color: rgb(6, 114, 6);
+}
+.boton__pdf {
+  color: rgb(184, 50, 50);
+}
+
+/* Tabla */
+/* Estilos para escritorio*/
+.tabla {
+  width: 70%;
+  margin: 0 auto;
+  border-collapse: collapse;
+  margin-bottom: 20px;
+  background-color: var(--blanco);
+}
+
+.dato__encabezado {
+  background-color: var(--primary-backgound);
+}
+
+.tabla__fila,
+.tabla__dato {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+}
+
+.nombre__campo {
+  font-weight: 900;
+  display: none;
+}
+
+
+.dato__encabezado {
+  font-weight: 900;
+  border: 1px solid #ddd;
+  padding: 5px;
+  text-align: left;
+}
+/* Estilos para dispositivos */
+@media (max-width: 768px) {
+  .tabla,
+  .dato__encabezado,
+  .tabla__contenido,
+  .tabla__fila,
+  .tabla__dato {
+    display: block;
+  }
+  .tabla {
+    background-color: unset;
+    width: 90%;
+  }
+
+  .nombre__campo {
+    font-weight: 900;
+    display: inline;
+    margin: 0;
+    padding: 0;
+  }
+  .tabla__ultimo {
+    margin-bottom: 1rem;
+  }
+
+  .dato__encabezado {
+    display: none;
+  }
+  .tabla__fila {
+    padding: 0;
+    border-radius: 5px;
+    background-color: var(--blanco);
+    margin-bottom: 1rem;
+    box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+  }
+
+  .tabla__dato {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    padding: 5px;
+    position: relative;
+  }
+ 
+  .fecha {
+    text-align: center;
+    padding: 10px;
+  }
+}
+
+/* validacion */
+.validacion__contenido {
+  margin-bottom: 8rem;
+}
+.validacion__heading {
+  font-size: 1.5rem;
+}
+.sinInfo{
+  color: rgb(240, 176, 176);
+}
 </style>
