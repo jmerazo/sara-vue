264e8f5948741c3f7d87025eba00f4115a147f35
jmerazo <55777009+jmerazo@users.noreply.github.com>
2024-05-30 00:16:28 -0500

    data sheet

--- src/helpers/exportDataSheet.js
+++ src/helpers/exportDataSheet.js
@@ -32,6 +32,7 @@ export const getFullImageUrl = (relativePath) => {
 };
 
 export const descargarPdfs = async (datos, tituloTabla) => {
+  console.log('datos: ', datos)
   const doc = new jsPDF({
     orientation: 'portrait',
     unit: 'mm',
@@ -41,44 +42,137 @@ export const descargarPdfs = async (datos, tituloTabla) => {
   const headerImage = await getBase64ImageFromUrl(headSheet);
   doc.addImage(headerImage, 'JPEG', 0, 0, doc.internal.pageSize.width, 30); // Ajusta el tamaño y posición según necesidad
 
-  let y = 32; // Inicia el contenido debajo del encabezado
-  const marginLeft = 10; // Margen izquierdo
-  const marginRight = 10; // Margen derecho
+  const pageHeight = doc.internal.pageSize.getHeight();
+  const pageWidth = doc.internal.pageSize.getWidth();
+  let marginLeft1 = 10; // Margen izquierdo para la primera columna
+  let marginLeft2 = 120; // Margen izquierdo para la segunda columna
+  const columnWidth = 90;
+  const marginTop = 40;
+  const marginBottom = pageHeight - 20;
+  const lineSpacing = 5; // Espacio entre líneas de texto
+  const startY = 42;
+  let y = startY;
+  let currentColumn = 1;
 
-  // TITLE DOCUMENT
+  // Renderizar título
   const titulo = `${datos.nombre_cientifico_especie} ${datos.nombre_autor_especie}`;
-  // Establece la fuente para medir el ancho del texto
-  doc.setFont("helvetica", "bolditalic"); // Negrita cursiva para el nombre científico
+  doc.setFont("helvetica", "bolditalic");
   const cientificoWidth = doc.getTextWidth(datos.nombre_cientifico_especie);
-  doc.setFont("helvetica", "bold"); // Negrita para el autor
+  doc.setFont("helvetica", "bold");
   const autorWidth = doc.getTextWidth(datos.nombre_autor_especie);
 
-  // Calcula la posición central para el conjunto
-  const pageWidth = doc.internal.pageSize.getWidth();
-  const pageHeight = doc.internal.pageSize.getHeight();
   const totalWidth = cientificoWidth + autorWidth;
   const startX = (pageWidth - totalWidth) / 2;
-  const midHeight = doc.internal.pageSize.getHeight() / 2;
-  const midWidth = doc.internal.pageSize.getWidth() / 2;
 
-  // Imprime primero el nombre científico
   doc.setFont("helvetica", "bolditalic");
   doc.text(datos.nombre_cientifico_especie, startX, 36);
-
-  // Después el nombre del autor
   doc.setFont("helvetica", "bold");
   doc.text(datos.nombre_autor_especie, startX + cientificoWidth + 2, 36);
 
-  // LINE MIDLE DOCUMENT 062A06
-  doc.setDrawColor(130, 148, 130); // Establece el color de la línea, negro por defecto
-  doc.setLineWidth(0.5); // Establece el grosor de la línea
-  doc.line(midWidth, 40, midWidth, pageHeight - 20);
+  // Línea de separación central
+  doc.setDrawColor(130, 148, 130);
+  doc.setLineWidth(0.5);
+  doc.line(pageWidth / 2, marginTop, pageWidth / 2, marginBottom);
 
-  // Imágenes adicionales si están disponibles
-  if (datos.img_general) {
-    const imgGeneral = await getBase64ImageFromUrl(getFullImageUrl(datos.img_general));
-    doc.addImage(imgGeneral, 'JPEG', 10, 100, 100, 75);
+   // Función para añadir título y texto justificado a la columna
+  const addTitleAndText = (title, text, x, y) => {
+    doc.setFont("helvetica", "bold");
+    doc.setFontSize(10);
+    doc.text(`${title}:`, x, y);
+    y += lineSpacing;
+
+    doc.setFont("helvetica", "normal");
+    if (title === 'SINONIMOS') {
+      const bulletText = text.split(',').map(item => `• ${item.trim()}`);
+      bulletText.forEach(line => {
+        if (y > marginBottom) {
+          y = switchColumnOrPage(x, y);
+        }
+        doc.text(line, x, y);
+        y += lineSpacing;
+      });
+    } else {
+      const splitText = doc.splitTextToSize(text, columnWidth);
+      splitText.forEach(line => {
+        if (y > marginBottom) {
+          y = switchColumnOrPage(x, y);
+        }
+        doc.text(line, x, y, { align: "justify" });
+        y += lineSpacing;
+      });
+    }
+    return y;
+  };
+
+  // Función para cambiar de columna o añadir una nueva página
+  const switchColumnOrPage = (x, y) => {
+    if (currentColumn === 1) {
+      // Cambiar a la segunda columna
+      currentColumn = 2;
+      return marginTop;
+    } else {
+      // Añadir una nueva página
+      doc.addPage();
+      currentColumn = 1;
+      /* addFooter(); // Añade el pie de página en cada nueva página */
+      return marginTop;
+    }
+  };
+
+  // Función para añadir imágenes
+  const addImage = async (imagePath, x, y) => {
+    const img = await getBase64ImageFromUrl(getFullImageUrl(imagePath));
+    doc.addImage(img, 'JPEG', x, y, 100, 75);
+    y += 85; // Espacio adicional después de la imagen
+    return y;
+  };
+
+  // Añadir datos a la columna actual
+  const propertiesToRender = [
+    { title: 'FAMILIA', key: 'familia' },
+    { title: 'CÓDIGO DE ESPECIE', key: 'cod_especie' },
+    { title: 'NOMBRE COMÚN', key: 'nom_comunes' },
+    { title: 'OTROS NOMBRES', key: 'otros_nombres' },
+    { title: 'DESCRIPCIÓN GENERAL', key: 'descripcion_general' },
+    { title: 'SINONIMOS', key: 'sinonimos' },
+    { title: 'DISTRIBUCIÓN', key: 'distribucion' },
+    { title: 'HOJAS', key: 'hojas' },
+    { title: 'FLORES', key: 'flores' },
+    { title: 'FRUTOS', key: 'frutos' },
+    { title: 'SEMILLAS', key: 'semillas' },
+    { title: 'USOS MADERABLES', key: 'usos_maderables' },
+    { title: 'USOS NO MADERABLES', key: 'usos_no_maderables' }
+  ];
+
+  for (const { title, key } of propertiesToRender) {
+    if (datos[key]) {
+      y = addTitleAndText(title, datos[key], currentColumn === 1 ? marginLeft1 : marginLeft2, y);
+    }
+
+    // Añadir imágenes específicas en las posiciones adecuadas
+    if (key === 'img_general' && datos.img_general) {
+      y = await addImage(datos.img_general, currentColumn === 1 ? marginLeft1 : marginLeft2, y);
+    } else if (key === 'img_leafs' && datos.img_leafs) {
+      y = await addImage(datos.img_leafs, currentColumn === 1 ? marginLeft1 : marginLeft2, y);
+    } else if (key === 'img_flowers' && datos.img_flowers) {
+      y = await addImage(datos.img_flowers, currentColumn === 1 ? marginLeft1 : marginLeft2, y);
+    } else if (key === 'img_fruits' && datos.img_fruits) {
+      y = await addImage(datos.img_fruits, currentColumn === 1 ? marginLeft1 : marginLeft2, y);
+    } else if (key === 'img_seeds' && datos.img_seeds) {
+      y = await addImage(datos.img_seeds, currentColumn === 1 ? marginLeft1 : marginLeft2, y);
+    }
+
+    // Si se pasa del margen inferior, cambiar a la segunda columna o nueva página
+    if (y > marginBottom) {
+      y = switchColumnOrPage(currentColumn === 1 ? marginLeft1 : marginLeft2, y);
+    }
   }
+  // Imágenes adicionales si están disponibles
+  /* if (datos.img_general) {
+    const imgGeneral = await getBase64ImageFromUrl(getFullImageUrl(datos.img_general));
+    doc.addImage(imgGeneral, 'JPEG', 10, y + 10, 100, 75);
+  } */
+
 
   // START FOOTER
   const textWidthFoot = pageWidth - 20; 
@@ -95,11 +189,11 @@ export const descargarPdfs = async (datos, tituloTabla) => {
   doc.setFontSize(8);
   doc.setTextColor(70, 70, 70);
   doc.setFont("helvetica", "normal");
-  doc.text(data1, marginLeft, pageHeight - 16);
-  doc.text(data2, marginLeft, pageHeight - 13);
-  doc.text(data3, marginLeft, pageHeight - 10);
-  doc.text(data4, marginLeft, pageHeight - 7);
-  doc.text(data5, marginLeft, pageHeight - 4);
+  doc.text(data1, 10, pageHeight - 16);
+  doc.text(data2, 10, pageHeight - 13);
+  doc.text(data3, 10, pageHeight - 10);
+  doc.text(data4, 10, pageHeight - 7);
+  doc.text(data5, 10, pageHeight - 4);
 
   doc.text(contac1, 140, pageHeight - 16);
   doc.text(contac2, 140, pageHeight - 13);
@@ -107,7 +201,7 @@ export const descargarPdfs = async (datos, tituloTabla) => {
   doc.text(contac4, 140, pageHeight - 7);
   // END FOOTER
 
-  doc.save(`${tituloTabla}.pdf`);
+  doc.save(`${datos.cod_especie}_${datos.nombre_cientifico}_${tituloTabla}.pdf`);
 };
 
 /* export const descargarPdfs = (datos, tituloTabla, columnas, inicio) => {
